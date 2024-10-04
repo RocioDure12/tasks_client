e9168f18c37527d8a7ce0d276b19418f758bfe1c
Rocio Dure <rocioevelyndure@gmail.com>
2024-10-04 16:23:05 -0300

    cambios en verify email

--- src/modules/pages/VerifyEmailAccount.tsx
+++ src/modules/pages/VerifyEmailAccount.tsx
@@ -1,12 +1,11 @@
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
 import useUserApi from "../hooks/useUserApi"
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 
 export default function VerifyEmailAccount() {
-     
-    //const [token, setToken]=useState("")
+
     const userApi=useUserApi()
     const navigate = useNavigate();
     const { token } = useParams<{ token: string }>();
@@ -20,22 +19,6 @@ export default function VerifyEmailAccount() {
         }
       }, [token]);
     
-
-
-        /*const handleChange=(evt:React.ChangeEvent<HTMLInputElement>)=>{
-            setToken(evt.target.value)
-
-    }
-
-    const handleSubmit=async(e: React.FormEvent<HTMLFormElement>)=>{
-        e.preventDefault()
-        const result=await userApi.verifyEmailAccount(token)
-        if (result.data){
-            navigate("/users/login")
-        }else{
-            console.log("error en verificacion del email")
-        }
-    }*/
 
     const handle_email_verification=async(token:string)=>{
         const result=await userApi.verifyEmailAccount(token)
