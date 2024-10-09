bcbe247ef15d948561c140401a82c84b341384c8
Rocio Dure <rocioevelyndure@gmail.com>
2024-10-09 11:09:12 -0300

    elimino cosas del home

--- src/modules/pages/Home.tsx
+++ src/modules/pages/Home.tsx
@@ -1,15 +1,9 @@
-import { useAppSelector } from "../../hooks";
-import { currentUser } from "../slices/authSlice";
-import useAuthApi from "../hooks/useAuthApi";
 import { useNavigate } from "react-router-dom";
 import { Calendar } from "@mantine/dates";
 import MainLayout from "../components/MainLayout";
 import { Card } from "../components/Card"
 
 export default function Home() {
-  const user = useAppSelector(currentUser);
-  const authApi = useAuthApi();
-  const navigate = useNavigate();
 
   return (
     <MainLayout>
@@ -20,10 +14,5 @@ export default function Home() {
     </MainLayout>
   );
 
-  /*const handleLogout=()=>{
-    const result= authApi.logout()
-    navigate('users/login')
-    return result
 
-}*/
 }
