5262c5f9f366514b8cb6f9b691539b4cdb7542d7
Rocio Dure <rocioevelyndure@gmail.com>
2024-11-04 11:21:55 -0300

    cambios en task user y subtask model

---
+++ src/modules/models/Subtask.ts
@@ -0,0 +1,11 @@
+interface Subtask{
+    id:number
+    subtask_name:string
+    status:boolean
+    task_id:number
+
+        
+    
+}
+
+export default Subtask
--- src/modules/models/Task.ts
+++ src/modules/models/Task.ts
@@ -1,13 +1,19 @@
+import Subtask from "./Subtask";
+
 interface Task{
     id: number;
     task_name: string;
     description: string;
     status:boolean;
     due_date:Date;
+    start_time?:Date;
+    end_time?:Date;
     deleted_at?:Date;
     created_at:Date;
     updated_at?:Date;
-    user_id?:number
+    user_id?:number;
+    category_id:number;
+    subtask?:Subtask
     
 }
 
