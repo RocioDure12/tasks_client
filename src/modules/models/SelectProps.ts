6e73383a657688662e3a65ebd37cb8091a8c9d06
Rocio Dure <rocioevelyndure@gmail.com>
2024-11-29 10:40:08 -0300

    select props model

---
+++ src/modules/models/SelectProps.ts
@@ -0,0 +1,12 @@
+interface SelectProps{
+    name:string
+    value?:string
+    onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+    options: { value: string; label: string }[];
+    placeholder?: string;
+    required?: boolean;
+    disabled?: boolean;
+    label?: string;
+
+
+}
