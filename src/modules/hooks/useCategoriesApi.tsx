3796864f66c959e796c75676686d9ef4deccd6c4
Rocio Dure <rocioevelyndure@gmail.com>
2024-12-23 10:27:52 -0300

    use categories api

---
+++ src/modules/hooks/useCategoriesApi.tsx
@@ -0,0 +1,31 @@
+import Category from "../models/Category";
+import useCrudApi from "../api/useCrudApi";
+import { ActionResult } from "../api/useApi";
+
+export default function useTaskApi() {
+    const api=useCrudApi<Category>("categories")
+
+    /*const getTaskById=async(id:number):Promise<ActionResult<Task>>=>{
+        return await api.read_by_id(id)
+    }*/
+
+    const createCategory=async (category: Partial<Category>): Promise<ActionResult<Category>> => {
+        return api.create(category as Category)
+    }
+
+    const readMyCategories= async (): Promise<ActionResult<Category[]>> => {
+        return api.getItems("/my_categories")
+
+    }
+    
+    const updateCategory=async (id:number,category:Category): Promise<ActionResult<Category>> => {
+        return api.update(id, category)
+    }
+
+    const deleteCategory=async(id:number):Promise<ActionResult<Category>>=>{
+        return api.deleteById(id)
+    }
+
+    return {createCategory, readMyCategories, updateCategory, deleteCategory}
+}
+    
--- src/modules/pages/Categories.tsx
+++ src/modules/pages/Categories.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react"
 import { Button } from "../components/Button"
 import { Input } from "../components/Input"
+import Category from "../models/Category";
+
 export const Categories:React.FC=()=>{
+const [categoriesList, setCategoriesList] = useState<Category[]>([]);
+
+  //const taskApi = useTaskApi();
+
 
     const handleChange=()=>{
 
     }
     return(
-    <>
-        <div className="m-2 rounded-lg bg-white  p-7 flex-row shadow-lg">
+    
+        <div className="m-2 rounded-lg bg-white  p-7 flex-col shadow-lg">
             <>
             <Input
             type="text"
@@ -31,6 +38,7 @@ export const Categories:React.FC=()=>{
             <div>
                 <ul>
                     <li>popo</li>
+                    <li>skjdf</li>
                 </ul>
                 
             </div>
@@ -41,7 +49,7 @@ export const Categories:React.FC=()=>{
         
         
        
-    </>
+
 
     )
 }
